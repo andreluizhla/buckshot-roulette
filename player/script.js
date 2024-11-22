dc38b7ea91a3aec97a3c9f6d96aae0e9b6bf8156
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2024-11-22 13:20:49 -0300

    site mostra os itens na tela

--- player/script.js
+++ player/script.js
@@ -10,7 +10,6 @@ if (!database) {
 }
 
 let div = document.getElementById('dados')
-let mesa = [[[[], []], [[], []]], [[[], []], [[], []]]]
 
 class Item {
     constructor(nome, src, descricao) {
@@ -31,35 +30,31 @@ var reverso = new Item('reverso', '../img/reverso.png', 'Inverte a direção que
 var bloqueio = new Item('bloqueio', '../img/bloqueio.png', 'Bloqueia a vez de quem você quizer')
 
 let itens = [adrenalina, celular, cerra, cigarro, heineken, lupa, paracetamol, reverso, bloqueio]
+let mesa = [[[[], []], [[], []]], [[[], []], [[], []]]]
 
-//var  = new Item('', '../img/.png', '')
-// let itens = {
-//     {'nome' : "adrenalina", 'src' = "../img/adrenalina.png", 'descricao' = "Esse item faz co mque você pegue imprestado (rouba) um item do seu amiguinho e usa imediatamente"},
-//     {'nome' = '', 'src' = '', 'img' = '', 'descricao' = ''}
-// } 
 
 document.getElementById('entrar').onclick = () => {
     div.innerHTML = ''
-    const player = window.prompt("Digite o seu nome para jogar:")
+    // const player = window.prompt("Digite o seu nome para jogar:")
     const salaId = window.prompt("Digite o código da sala:")
-
-
+    
+    
     // Caminho onde os dados estão armazenados:
     const salaRef = ref(database, 'salas/', salaId);  
-
+    
     // const salaRef = database.colection('salas').doc(salaId)
     
     // update(salaRef, {
-    //     
-    //     jogador : firebase.firestore.FieldValue.arreyUnion(player),
-    //     status: "andamento"
-    // })
-    
+        //     
+        //     jogador : firebase.firestore.FieldValue.arreyUnion(player),
+        //     status: "andamento"
+        // })
+        
     div.innerHTML += `<p>Nome : ${player}</p>`
     div.innerHTML += `<p>Sala do jogo : ${salaId}</p>`
     div.innerHTML += `<p>Sala de referência : ${salaRef}</p>`
-
-
+    
+    
     get(salaRef).then((snapshot) => {
         if (snapshot.exists()) {
             div.innerHTML += snapshot.val()  // Exibe os dados da sala
@@ -69,23 +64,32 @@ document.getElementById('entrar').onclick = () => {
         }
     }).catch((error) => {
         div.innerHTML += "Erro ao ler dados: ", error
-        });
-
+    });
+    
     salaRef.update({
         //jogador : 
     })
 }
-
-document.getElementById('criar-itens').onclick = () => {
-    let item_aleatorio 
     
+document.getElementById('criar-itens').onclick = () => {
+    let img_itens = document.querySelectorAll('.item')
+    let index_aleatorio 
+    let item_aleatorio
+    let cont = 0
     for (let lado = 0; lado < 2 ; lado++){
         for (let linha = 0; linha < 2; linha++){
             for (let coluna = 0; coluna < 2; coluna++){
-                item_aleatorio = Math.floor(Math.random() * itens.length)
-                mesa[lado][linha][coluna].push(itens[item_aleatorio])
+                index_aleatorio = Math.floor(Math.random() * itens.length)
+                item_aleatorio = itens[index_aleatorio]
+                mesa[lado][linha][coluna].push(item_aleatorio)
+                img_itens[cont].src = item_aleatorio.src
+                img_itens[cont].alt = `${item_aleatorio.nome}: ${item_aleatorio.descricao}`
+                
+                cont++
             }
         }
     }
     console.log(mesa)
 }
+
+// document.
